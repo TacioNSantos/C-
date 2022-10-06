7005be6ef8ba60fd8a86597a7f76c573b222082b
TacioNSantos <101426873+TacioNSantos@users.noreply.github.com>
2022-10-06 08:11:01 -0300

    Add files via upload

---
+++ Aula 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int soma = 0;
+            for (int i = 0; i < 10; i++) //inicializador da condição
+            {
+                soma += 1; // soma = soma + 1;
+                Console.WriteLine(soma);
+            }
+            */
+            int valor = 1, soma = 0;
+            Console.WriteLine("Imfome os valores, digite 0 para sair!");
+            while (valor != 0)
+            {
+                valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }       
+            Console.WriteLine(soma);
+        }
+    }
+}
