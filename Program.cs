d59b84ceb51897ff982aedad6a17c469537c91dc
TacioNSantos <101426873+TacioNSantos@users.noreply.github.com>
2022-10-06 13:24:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Imforme o valor do fatorial");
+            int valor = int.Parse(Console.ReadLine());
+            int n = valor;
+
+            while (valor >= 1)
+            {
+            valor = valor * (n-1);
+                n --;
+            Console.WriteLine(valor);
+            }
+        }
+    }
+}
