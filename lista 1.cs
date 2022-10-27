6ce77e603c4e62d50b03455c734b5fe0e5ef8eff
TacioNSantos <101426873+TacioNSantos@users.noreply.github.com>
2022-10-27 16:03:09 -0300

    Update and rename Aula 2.cs to lista 1.cs

---
+++ lista 1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int IdadeJoao = 15;
+int IdadeMaria = 30;
+int IdadeMarcelo = 20;
+float resultado;
+
+resultado = ((IdadeJoao + IdadeMarcelo + IdadeMaria) / 3);
+Console.WriteLine(resultado);
+*/
+
+//////////////////////////////////////////////////////////////////
+
+int a = 3;
+int b = 6;
+int c = 7;
+
+double delta = (b * b - 4 * a * c);
+double raiz = Math.Sqrt(delta);
+
+double x1 = ((-b) + raiz) / (2 * a);
+double x2 = ((-b) - raiz) / (2 * a);
+
+Console.WriteLine(x1);
+Console.WriteLine(x2);
+Console.ReadKey();
+
+///////////////////////////////////////////////
+
+/* float p, a, IMC;
+
+Console.WriteLine("Imforme seu peso");
+p = float.Parse(Console.ReadLine());
+Console.WriteLine("Imforme sua altura");
+a = float.Parse(Console.ReadLine());
+
+IMC = (p / (a*a));
+Console.WriteLine(IMC);
+*/
+        }
+    }
+}
